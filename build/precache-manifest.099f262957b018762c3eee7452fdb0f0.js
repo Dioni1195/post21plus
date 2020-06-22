9ec829de44f8c3d3b99326afe1725c0cab309733
Dioni1195 <dionisio.arango95@gmail.com>
2020-06-22 15:11:27 -0500

    Fix env vars with react prefix

---
+++ build/precache-manifest.099f262957b018762c3eee7452fdb0f0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f02325471b22253f5879cea552fa4ef",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ca772cfb0670ec51eb80",
+    "url": "/static/css/main.53535a79.chunk.css"
+  },
+  {
+    "revision": "201d8a620d624d1c2cd9",
+    "url": "/static/js/2.bb6734d5.chunk.js"
+  },
+  {
+    "revision": "c21964cfdb755a684c1430aee2a4d7bc",
+    "url": "/static/js/2.bb6734d5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ca772cfb0670ec51eb80",
+    "url": "/static/js/main.77ffaa1d.chunk.js"
+  },
+  {
+    "revision": "8394b4dc22e6a111b8ed",
+    "url": "/static/js/runtime-main.fcbed664.js"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.5bbd04b7c4138ec4fef6ce9884425683.js"
+  "/precache-manifest.099f262957b018762c3eee7452fdb0f0.js"
 );
 
 self.addEventListener('message', (event) => {
